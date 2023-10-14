8488ed26496e98d86147a91753a68204d701ceb6
Valentin Fernandez Radovich <fernandezradovich@gmail.com>
2023-10-14 11:28:15 -0300

    data logic in order to get last day

--- interface/main.py
+++ interface/main.py
@@ -8,7 +8,7 @@ from ml_logic.registry import *
 from ml_logic.visualizations import plot_neg_pos, plot_bar_hor,stacked_bars
 import pandas as pd
 
-def analysis(candidate, time_filter) -> pd.DataFrame:
+def analysis_old(candidate, time_filter) -> pd.DataFrame:
     """
     Main function to run the complete analysis.
     Via GCS or running the whole analysis.
@@ -69,6 +69,114 @@ def analysis(candidate, time_filter) -> pd.DataFrame:
 
         return final_df
 
+def analysis(candidate, time_filter='day'):
+    """
+    Main function to run the complete analysis.
+    Via GCS or running the whole analysis.
+    """
+    try:
+        print(f"\n⭐️ Use case: Sentiment Analysis - GCS")
+        print(f"⭐️ Candidate: {candidate}")
+        print(f"⭐️ Time filter: month (but not today)")
+
+        main_df = load_insights(candidate=candidate)
+
+        print("⭐️ Main DF Returned!", main_df.shape)
+
+    except:
+        print("\n⭐️ Use case: Sentiment Analysis - Running")
+        print("\n⭐️ Getting whole month including today...")
+        print(f"⭐️ Candidate: {candidate}")
+        print(f"⭐️ Time filter: month")
+
+        print("⭐️ Getting data from API and cleaning it...")
+
+        # getting raw data
+        processed_data = get_clean_reddits(candidate=candidate, time_filter='month')
+
+        print("⭐️ Loading models...")
+        print(f"First model: {MODEL_PATH_SENTIMENT}")
+        print(f"Second model: {MODEL_PATH_EMOTION}")
+
+        # loading models
+        model_sentiment = load_model(MODEL_PATH_SENTIMENT, tokenizer=True)
+        model_emotion = load_model(MODEL_PATH_EMOTION, tokenizer=False)
+
+        assert model_sentiment is not None
+        assert model_emotion is not None
+
+        # sentiment analysis
+        print("\n⭐️ Sentiment Analysis")
+        print("⭐️ Classifying sentiment...")
+        aux_df = text_sentiment_classifier(processed_data, model_sentiment, only_neutrals=False)
+        aux_df = text_sentiment_classifier(aux_df, model_emotion, only_neutrals=True)
+
+        # processing sentiment labels
+        print("⭐️ Processing sentiment labels...")
+        aux_df = process_sentiment_labels(aux_df)
+
+        # encoding sentiment labels
+        print("⭐️ Encoding sentiment labels...")
+        encoded_df = encode_sentiment_labels(aux_df)
+
+        # getting insights
+        print("⭐️ Getting insights...")
+        main_df = get_insights(encoded_df)
+
+        # saving insights
+        save_insights(main_df, candidate=candidate)
+
+        print("⭐️ Final DF Returned!", main_df.shape)
+
+        return main_df
+
+    print("\n📍 Adding today's data...")
+    print("\n📍 Use case: Sentiment Analysis - Running")
+    print("\n📍 Getting todays data...")
+    print(f"📍 Candidate: {candidate}")
+    print(f"📍 Time filter: day")
+
+    print("📍 Getting data from API and cleaning it...")
+
+    # getting raw data
+    processed_data = get_clean_reddits(candidate=candidate, time_filter=time_filter)
+
+    print("📍 Loading models...")
+    print(f"First model: {MODEL_PATH_SENTIMENT}")
+    print(f"Second model: {MODEL_PATH_EMOTION}")
+
+    # loading models
+    model_sentiment = load_model(MODEL_PATH_SENTIMENT, tokenizer=True)
+    model_emotion = load_model(MODEL_PATH_EMOTION, tokenizer=False)
+
+    assert model_sentiment is not None
+    assert model_emotion is not None
+
+    # sentiment analysis
+    print("\n📍 Sentiment Analysis")
+    print("📍 Classifying sentiment...")
+    aux_df = text_sentiment_classifier(processed_data, model_sentiment, only_neutrals=False)
+    aux_df = text_sentiment_classifier(aux_df, model_emotion, only_neutrals=True)
+
+    # processing sentiment labels
+    print("📍 Processing sentiment labels...")
+    aux_df = process_sentiment_labels(aux_df)
+
+    # encoding sentiment labels
+    print("📍 Encoding sentiment labels...")
+    encoded_df = encode_sentiment_labels(aux_df)
+
+    # getting insights
+    print("📍 Getting insights...")
+    today_df = get_insights(encoded_df)
+
+    print("📍 Today's DF: ", today_df.shape)
+    print("Mergin with main DF...")
+    new_df = merging_today_with_month_df(today_df, main_df)
+    save_insights(new_df, candidate=candidate)
+
+    return new_df
+
 def visualizations():
     pass
 
@@ -76,13 +184,10 @@ if __name__ == "__main__":
     ### the next flow should be executed once a day (23:50) ###
     candidates_dfs = []
     for candidate in CANDIDATES_LIST:
-        df = analysis(candidate=candidate, time_filter='month')
+        # df = analysis_old(candidate=candidate, time_filter='month')
+        df = analysis(candidate=candidate)
         plot_neg_pos(df, candidate)
         plot_bar_hor(df, candidate)
         candidates_dfs.append(df)
 
     stacked_bars(candidates_dfs)
-
-    # nexts steps:
-    # get the insights from today and replace the insights CSV in GCS
-    # visualizations functions in order to save the plots imgs in GCP
--- ml_logic/data.py
+++ ml_logic/data.py
@@ -5,53 +5,6 @@ from utils import *
 from ml_logic.preprocessors import *
 from params import *
 
-
-def get_raw_news(candidate) -> pd.DataFrame:
-    """
-    Getting news based on candidate name
-
-    Keyword arguments:
-    candidate -- candidate name
-
-    Return: df API content
-    """
-
-    base_url = "https://newsapi.org/v2/everything?"
-
-    params = {
-        "apiKey": NEWS_KEY,
-        "q": candidate,
-        "searchIn": ["title","description"],
-        "domains": ['lanacion.com.ar, infobae.com'],
-        "from": datetime.datetime.now() - datetime.timedelta(days=30)
-    }
-
-    response = requests.get(base_url, params=params)
-    content = response.json()
-
-    df = pd.DataFrame(content['articles'])
-    df = df[['publishedAt', 'title', 'description', 'content']]
-    df = df.sort_values(by=['publishedAt'], ascending=False)
-    df = df.reset_index(drop=True)
-
-    return df
-
-def get_clean_news(candidate) -> pd.DataFrame:
-    """
-    Getting news based on candidate name
-
-    Keyword arguments:
-    candidate -- candidate name
-
-    Return: df Cleaned data
-    """
-    print(f"Getting raw news for {candidate}...")
-    df = get_raw_news(candidate)
-
-    print(f"Processing news for {candidate}...")
-    df = process_news_data(df)
-    return df
-
 def get_raw_reddits(subreddit, candidate, time_filter) -> pd.DataFrame:
     """Getting raw data from reddit API.
 
